f7a0ef1c4b0aecd9964ec7e59371724beb78a017
FreddyPashley <fredpashley1@gmail.com>
2024-01-26 10:51:40 +0000

    Fixes

--- lfreq.py
+++
@@ -1,9 +0,0 @@
-cs = " ABCDEFGHIJKLMNOPQRSTUVWYZabcdefghijklmnopqrstuvwxyz0123456789.!?,:;'@[]{}\"£$%^&*()/*-+_=`¬\\|"
-
-c = "|"
-
-print(cs.index("|"))
-
-freq = (cs.index(c)+1)*100 + 99
-
-print(freq)
